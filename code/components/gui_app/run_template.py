a479e5c247ccb00882797a6c02c4e26b0642e2c2
Jak0x42 <jacobbarany@gmail.com>
2024-10-03 14:22:18 +1000

    Improve the GUI, now works with filtering, styling also improved.

--- code/components/gui_app/run_template.py
+++ code/components/gui_app/run_template.py
@@ -9,108 +9,276 @@ from data_handler.data_handler import DataHandler
 class MainFrame(BaseFrame):
     def __init__(self):
         super().__init__(None)
-
         self.search_handler = SearchHandler()
         self.data_handler = DataHandler()
 
-        self.m_listBox2.Hide()
+        self.m_panel_filter.Hide()
+        self.m_panel_search.Show()
 
-        self.m_textCtrl4.Bind(wx.EVT_TEXT, self.on_text_input)
+        self.m_grid_search_result.Hide()
+        self.m_grid_filter_result.Hide()
+        self.m_staticText_search_result.Hide()
+        self.m_staticText_filter_result.Hide()
 
-        self.m_listBox2.Bind(wx.EVT_LISTBOX, self.on_listbox_select)
+        self.m_listBox_food_search.Hide()
+        
+        self.m_grid_search_result.SetRowLabelSize(0)
 
-        self.m_button3.Bind(wx.EVT_BUTTON, self.on_button_click)
+        self.m_searchCtrl_food_search.Bind(wx.EVT_TEXT, self.on_text_input)
+        self.m_searchCtrl_food_search.Bind(wx.EVT_SEARCHCTRL_SEARCH_BTN, self.on_search_button)
+        self.m_listBox_food_search.Bind(wx.EVT_LISTBOX, self.on_listbox_select)
+
+        self.m_button_enable_filtering.Bind(wx.EVT_BUTTON, self.on_enable_filtering)
+        self.m_button_enable_searching.Bind(wx.EVT_BUTTON, self.on_enable_searching)
 
         self.Bind(wx.EVT_LEFT_DOWN, self.on_frame_click)
+        self.m_searchCtrl_food_search.Bind(wx.EVT_LEFT_DOWN, self.on_text_or_list_click)
+        self.m_listBox_food_search.Bind(wx.EVT_LEFT_DOWN, self.on_text_or_list_click)
 
-        self.m_textCtrl4.Bind(wx.EVT_LEFT_DOWN, self.on_text_or_list_click)
-        self.m_listBox2.Bind(wx.EVT_LEFT_DOWN, self.on_text_or_list_click)
+        self.populate_nutrient_choices()
+
+        self.m_checkBox_level_filter.Bind(wx.EVT_CHECKBOX, self.on_level_filter_checked)
+        self.m_checkBox_range_filter.Bind(wx.EVT_CHECKBOX, self.on_range_filter_checked)
+        self.m_choice_nutrient_filter.Bind(wx.EVT_CHOICE, self.on_nutrient_selected)
+        self.m_choice_filter_level.Bind(wx.EVT_CHOICE, self.on_level_selected)
+        self.m_spinCtrlDouble_range_min.Bind(wx.EVT_SPINCTRLDOUBLE, self.on_range_values_changed)
+        self.m_spinCtrlDouble_range_max.Bind(wx.EVT_SPINCTRLDOUBLE, self.on_range_values_changed)
+
+        self.m_spinCtrlDouble_range_min.Disable()
+        self.m_spinCtrlDouble_range_max.Disable()
+        self.m_choice_filter_level.Disable()
 
         self.is_typing = True
-
-        self.setup_grid()
-
         self.Show()
 
-    def setup_grid(self):
-        self.m_grid3.ClearGrid()
+    def on_enable_filtering(self, event):
+        self.m_panel_search.Hide()
+        self.m_panel_filter.Show()
+        self.Layout()
 
-        columns = [col for col in self.search_handler.database_df.columns if col != 'food']
+    def on_enable_searching(self, event):
+        self.m_panel_filter.Hide()
+        self.m_panel_search.Show()
+        self.Layout()
 
-        self.m_grid3.DeleteCols(0, self.m_grid3.GetNumberCols())
-        self.m_grid3.AppendCols(len(columns))
-        for i, column in enumerate(columns):
-            self.m_grid3.SetColLabelValue(i, column)
+    def populate_nutrient_choices(self):
+        nutrients = [col for col in self.search_handler.database_df.columns if col != 'food']
+        self.m_choice_nutrient_filter.AppendItems(nutrients)
 
-        self.m_grid3.SetRowLabelSize(0)
+    def on_level_filter_checked(self, event):
+        if self.m_checkBox_level_filter.IsChecked():
+            self.m_checkBox_range_filter.SetValue(False)
+            self.m_choice_filter_level.Enable()
+            self.m_spinCtrlDouble_range_min.Disable()
+            self.m_spinCtrlDouble_range_max.Disable()
+        else:
+            self.m_choice_filter_level.Disable()
+        self.apply_filter()
 
-        self.m_grid3.AutoSizeColumns()
+    def on_range_filter_checked(self, event):
+        if self.m_checkBox_range_filter.IsChecked():
+            self.m_checkBox_level_filter.SetValue(False)
+            self.m_spinCtrlDouble_range_min.Enable()
+            self.m_spinCtrlDouble_range_max.Enable()
+            self.m_choice_filter_level.Disable()
+        else:
+            self.m_spinCtrlDouble_range_min.Disable()
+            self.m_spinCtrlDouble_range_max.Disable()
+        self.apply_filter()
+
+    def on_nutrient_selected(self, event):
+        self.apply_filter()
+
+    def on_level_selected(self, event):
+        self.apply_filter()
+
+    def on_range_values_changed(self, event):
+        self.apply_filter()
+
+    def apply_filter(self):
+        nutrient = self.m_choice_nutrient_filter.GetStringSelection()
+        if not nutrient:
+            self.clear_filter_grid()
+            return
+
+        if self.m_checkBox_level_filter.IsChecked():
+            level = self.m_choice_filter_level.GetStringSelection().lower()
+            if not level:
+                self.clear_filter_grid()
+                return
+            filtered_df = self.search_handler.filter_by_level(nutrient, level)
+        elif self.m_checkBox_range_filter.IsChecked():
+            min_val = self.m_spinCtrlDouble_range_min.GetValue()
+            max_val = self.m_spinCtrlDouble_range_max.GetValue()
+            if min_val > max_val:
+                wx.MessageBox("Minimum value cannot be greater than maximum value.", "Error", wx.OK | wx.ICON_ERROR)
+                return
+            filtered_df = self.search_handler.filter_by_range(nutrient, min_val, max_val)
+        else:
+            self.clear_filter_grid()
+            return
+
+        if filtered_df.empty:
+            wx.MessageBox("No matching foods found.", "Information", wx.OK | wx.ICON_INFORMATION)
+            self.clear_filter_grid()
+        else:
+            self.update_filter_grid(filtered_df, nutrient)
+
+    def update_filter_grid(self, df, nutrient):
+        formatted_nutrient = ' '.join(word.capitalize() for word in nutrient.split())
+        self.m_staticText_filter_result.SetLabel("Foods containing " + formatted_nutrient + " within the filter criteria")
+        self.m_staticText_filter_result.Show()
+        grid = self.m_grid_filter_result
+
+        grid.Show()
+
+        grid.ClearGrid()
+        num_rows = grid.GetNumberRows()
+        num_cols = grid.GetNumberCols()
+        if num_rows > 0:
+            grid.DeleteRows(0, num_rows, True)
+        if num_cols > 0:
+            grid.DeleteCols(0, num_cols, True)
+
+        food_names = df['food'].tolist()
+        grid.AppendCols(len(food_names))
+        grid.AppendRows(1)
+
+        for col_idx, food in enumerate(food_names):
+            grid.SetColLabelValue(col_idx, food)
+
+        grid.SetRowLabelValue(0, nutrient)
+
+        grid.SetRowLabelSize(wx.grid.GRID_AUTOSIZE)
+        grid.AutoSizeRowLabelSize(0)
+
+        for col_idx, value in enumerate(df[nutrient]):
+            grid.SetCellValue(0, col_idx, str(value))
+
+        grid.AutoSizeColumns()
+        grid.AutoSizeRows()
+        self.Layout()
+        grid.ForceRefresh()
+
+    def clear_filter_grid(self):
+        grid = self.m_grid_filter_result
+        grid.ClearGrid()
+        num_rows = grid.GetNumberRows()
+        num_cols = grid.GetNumberCols()
+        if num_rows > 0:
+            grid.DeleteRows(0, num_rows, True)
+        if num_cols > 0:
+            grid.DeleteCols(0, num_cols, True)
+        grid.Hide()
+        self.Layout()
+        grid.ForceRefresh()
 
     def on_text_input(self, event):
         if not self.is_typing:
             return
 
-        query = self.m_textCtrl4.GetValue()
+        query = self.m_searchCtrl_food_search.GetValue()
         if query:
             matches = self.get_similar_items(query)
             if not matches.empty:
                 self.update_list(matches['food'].tolist())
-                self.m_listBox2.Show()
+                self.m_listBox_food_search.Show()
             else:
-                self.m_listBox2.Hide()
+                self.m_listBox_food_search.Hide()
         else:
-            self.m_listBox2.Hide()
-
+            self.m_listBox_food_search.Hide()
         self.Layout()
 
     def get_similar_items(self, query):
         return self.search_handler.search_food(query)
 
     def update_list(self, matches):
-        self.m_listBox2.Clear()
-        self.m_listBox2.Append(matches)
+        self.m_listBox_food_search.Clear()
+        self.m_listBox_food_search.Append(matches)
 
     def on_listbox_select(self, event):
-        selected_item = self.m_listBox2.GetString(self.m_listBox2.GetSelection())
+        selected_item = self.m_listBox_food_search.GetString(self.m_listBox_food_search.GetSelection())
         self.is_typing = False
-
-        self.m_textCtrl4.SetValue(selected_item)
-
+        self.m_searchCtrl_food_search.SetValue(selected_item)
         self.is_typing = True
 
-    def on_button_click(self, event):
-        selected_food = self.m_textCtrl4.GetValue()
-        food_item = self.search_handler.get_food_item(selected_food)
+    def on_search_button(self, event):
+        selected_food = self.m_searchCtrl_food_search.GetValue()
+        self.perform_search(selected_food)
+        self.m_listBox_food_search.Hide()
+        self.Layout()
+
+    def perform_search(self, query):
+        food_item = self.search_handler.get_food_item(query)
         if food_item is not None:
-            self.update_grid(food_item)
+            self.update_search_grid(food_item)
         else:
-            wx.MessageBox(f"No information found for '{selected_food}'", "Food Not Found", wx.OK | wx.ICON_WARNING)
+            wx.MessageBox(f"No information found for '{query}'", "Food Not Found", wx.OK | wx.ICON_WARNING)
+            self.m_staticText_search_result.Hide()
+            self.clear_search_grid()
+        self.m_listBox_food_search.Hide()
+        self.Layout()
 
-    def update_grid(self, food_item):
-        self.m_grid3.DeleteRows(0, self.m_grid3.GetNumberRows())
-        self.m_grid3.AppendRows(1)
+    def update_search_grid(self, food_item):
+        food_name = food_item['food']
+        formatted_name = ' '.join(word.capitalize() for word in food_name.split())
+        self.m_staticText_search_result.SetLabel(formatted_name + " Nutrition Information")
+        self.m_staticText_search_result.Show()
+        grid = self.m_grid_search_result
 
-        col = 0
-        for nutrient, value in food_item.items():
-            if nutrient != 'food':
-                self.m_grid3.SetCellValue(0, col, str(value))
-                col += 1
+        grid.Show()
 
-        self.m_grid3.AutoSizeRows()
+        grid.ClearGrid()
+        num_rows = grid.GetNumberRows()
+        num_cols = grid.GetNumberCols()
+        if num_rows > 0:
+            grid.DeleteRows(0, num_rows, True)
+        if num_cols > 0:
+            grid.DeleteCols(0, num_cols, True)
+
+        nutrients = [col for col in food_item.index if col != 'food']
+        grid.AppendCols(len(nutrients))
+        grid.AppendRows(1)
+
+        for col_idx, col_name in enumerate(nutrients):
+            grid.SetColLabelValue(col_idx, col_name)
+
+        grid.SetRowLabelValue(0, "")
+
+
+
+        for col_idx, col_name in enumerate(nutrients):
+            value = str(food_item[col_name])
+            grid.SetCellValue(0, col_idx, value)
+
+        grid.AutoSizeColumns()
+        self.Layout()
+        grid.ForceRefresh()
+
+    def clear_search_grid(self):
+        grid = self.m_grid_search_result
+        grid.ClearGrid()
+        num_rows = grid.GetNumberRows()
+        num_cols = grid.GetNumberCols()
+        if num_rows > 0:
+            grid.DeleteRows(0, num_rows, True)
+        if num_cols > 0:
+            grid.DeleteCols(0, num_cols, True)
+        grid.Hide()
+        self.Layout()
+        grid.ForceRefresh()
 
     def on_frame_click(self, event):
         pos = event.GetPosition()
-
-        if not self.m_textCtrl4.GetScreenRect().Contains(self.ScreenToClient(wx.GetMousePosition())) and \
-           not self.m_listBox2.GetScreenRect().Contains(self.ScreenToClient(wx.GetMousePosition())):
-            self.m_listBox2.Hide()
+        if not self.m_searchCtrl_food_search.GetScreenRect().Contains(self.ScreenToClient(wx.GetMousePosition())) and \
+           not self.m_listBox_food_search.GetScreenRect().Contains(self.ScreenToClient(wx.GetMousePosition())):
+            self.m_listBox_food_search.Hide()
             self.Layout()
-        
         event.Skip()
 
     def on_text_or_list_click(self, event):
-        if self.m_textCtrl4.GetValue() and not self.m_listBox2.IsShown():
-            self.m_listBox2.Show()
+        if self.m_searchCtrl_food_search.GetValue() and not self.m_listBox_food_search.IsShown():
+            self.m_listBox_food_search.Show()
             self.Layout()
         event.Skip()
 
--- code/components/gui_app/template_frame.py
+++ code/components/gui_app/template_frame.py
@@ -21,71 +21,228 @@ _ = gettext.gettext
 class MyFrame1 ( wx.Frame ):
 
     def __init__( self, parent ):
-        wx.Frame.__init__ ( self, parent, id = wx.ID_ANY, title = wx.EmptyString, pos = wx.DefaultPosition, size = wx.Size( 969,676 ), style = wx.DEFAULT_FRAME_STYLE|wx.TAB_TRAVERSAL )
+        wx.Frame.__init__ ( self, parent, id = wx.ID_ANY, title = wx.EmptyString, pos = wx.DefaultPosition, size = wx.Size( 1189,757 ), style = wx.DEFAULT_FRAME_STYLE|wx.TAB_TRAVERSAL )
 
         self.SetSizeHints( wx.DefaultSize, wx.DefaultSize )
 
-        bSizer1 = wx.BoxSizer( wx.HORIZONTAL )
+        bSizer1 = wx.BoxSizer( wx.VERTICAL )
 
-        fgSizer2 = wx.FlexGridSizer( 0, 3, 0, 0 )
-        fgSizer2.SetFlexibleDirection( wx.BOTH )
-        fgSizer2.SetNonFlexibleGrowMode( wx.FLEX_GROWMODE_ALL )
+        fgSizer3 = wx.FlexGridSizer( 0, 5, 0, 0 )
+        fgSizer3.SetFlexibleDirection( wx.HORIZONTAL )
+        fgSizer3.SetNonFlexibleGrowMode( wx.FLEX_GROWMODE_SPECIFIED )
 
-        self.m_staticText3 = wx.StaticText( self, wx.ID_ANY, _(u"Search Food:"), wx.DefaultPosition, wx.DefaultSize, 0 )
-        self.m_staticText3.Wrap( -1 )
+        bSizer13 = wx.BoxSizer( wx.HORIZONTAL )
 
-        fgSizer2.Add( self.m_staticText3, 0, wx.LEFT|wx.TOP, 5 )
+        self.m_panel_search = wx.Panel( self, wx.ID_ANY, wx.DefaultPosition, wx.DefaultSize, wx.TAB_TRAVERSAL )
+        bSizer17 = wx.BoxSizer( wx.VERTICAL )
 
-        bSizer7 = wx.BoxSizer( wx.VERTICAL )
+        sbSizer1 = wx.StaticBoxSizer( wx.StaticBox( self.m_panel_search, wx.ID_ANY, wx.EmptyString ), wx.HORIZONTAL )
 
-        self.m_textCtrl4 = wx.TextCtrl( self, wx.ID_ANY, wx.EmptyString, wx.DefaultPosition, wx.DefaultSize, 0 )
-        self.m_textCtrl4.SetMinSize( wx.Size( 300,-1 ) )
-        self.m_textCtrl4.SetMaxSize( wx.Size( 300,-1 ) )
+        bSizer131 = wx.BoxSizer( wx.VERTICAL )
 
-        bSizer7.Add( self.m_textCtrl4, 0, wx.ALIGN_CENTER_HORIZONTAL|wx.LEFT|wx.RIGHT|wx.TOP, 5 )
+        self.m_searchCtrl_food_search = wx.SearchCtrl( sbSizer1.GetStaticBox(), wx.ID_ANY, wx.EmptyString, wx.DefaultPosition, wx.DefaultSize, wx.TE_CENTER )
+        self.m_searchCtrl_food_search.ShowSearchButton( True )
+        self.m_searchCtrl_food_search.ShowCancelButton( True )
+        self.m_searchCtrl_food_search.SetMinSize( wx.Size( 300,-1 ) )
+        self.m_searchCtrl_food_search.SetMaxSize( wx.Size( 300,-1 ) )
 
-        m_listBox2Choices = []
-        self.m_listBox2 = wx.ListBox( self, wx.ID_ANY, wx.DefaultPosition, wx.DefaultSize, m_listBox2Choices, 0 )
-        self.m_listBox2.SetMinSize( wx.Size( 300,-1 ) )
-        self.m_listBox2.SetMaxSize( wx.Size( 300,-1 ) )
+        bSizer131.Add( self.m_searchCtrl_food_search, 0, wx.LEFT|wx.RIGHT|wx.TOP, 5 )
 
-        bSizer7.Add( self.m_listBox2, 0, wx.ALIGN_CENTER_HORIZONTAL|wx.LEFT|wx.RIGHT, 5 )
+        m_listBox_food_searchChoices = []
+        self.m_listBox_food_search = wx.ListBox( sbSizer1.GetStaticBox(), wx.ID_ANY, wx.DefaultPosition, wx.DefaultSize, m_listBox_food_searchChoices, 0 )
+        self.m_listBox_food_search.SetMinSize( wx.Size( 300,100 ) )
+        self.m_listBox_food_search.SetMaxSize( wx.Size( 300,100 ) )
 
-
-        fgSizer2.Add( bSizer7, 1, 0, 5 )
-
-        self.m_button3 = wx.Button( self, wx.ID_ANY, _(u"Search"), wx.DefaultPosition, wx.DefaultSize, 0 )
-        fgSizer2.Add( self.m_button3, 0, wx.ALIGN_LEFT, 5 )
+        bSizer131.Add( self.m_listBox_food_search, 0, wx.LEFT|wx.RESERVE_SPACE_EVEN_IF_HIDDEN|wx.RIGHT, 5 )
 
 
-        bSizer1.Add( fgSizer2, 1, 0, 5 )
+        sbSizer1.Add( bSizer131, 1, wx.FIXED_MINSIZE, 5 )
 
-        self.m_grid3 = wx.grid.Grid( self, wx.ID_ANY, wx.DefaultPosition, wx.DefaultSize, 0 )
+        self.m_button_enable_filtering = wx.Button( sbSizer1.GetStaticBox(), wx.ID_ANY, _(u"Enable Filtering"), wx.DefaultPosition, wx.DefaultSize, 0 )
+        sbSizer1.Add( self.m_button_enable_filtering, 0, wx.ALL, 5 )
+
+
+        bSizer17.Add( sbSizer1, 1, 0, 5 )
+
+        bSizer_search_results = wx.BoxSizer( wx.VERTICAL )
+
+        self.m_staticText_search_result = wx.StaticText( self.m_panel_search, wx.ID_ANY, _(u"Food"), wx.DefaultPosition, wx.DefaultSize, 0 )
+        self.m_staticText_search_result.Wrap( -1 )
+
+        bSizer_search_results.Add( self.m_staticText_search_result, 0, wx.ALIGN_CENTER_HORIZONTAL|wx.ALL, 5 )
+
+        self.m_grid_search_result = wx.grid.Grid( self.m_panel_search, wx.ID_ANY, wx.DefaultPosition, wx.DefaultSize, 0 )
 
         # Grid
-        self.m_grid3.CreateGrid( 1, 34 )
-        self.m_grid3.EnableEditing( False )
-        self.m_grid3.EnableGridLines( True )
-        self.m_grid3.EnableDragGridSize( False )
-        self.m_grid3.SetMargins( 0, 0 )
+        self.m_grid_search_result.CreateGrid( 1, 5 )
+        self.m_grid_search_result.EnableEditing( False )
+        self.m_grid_search_result.EnableGridLines( True )
+        self.m_grid_search_result.EnableDragGridSize( False )
+        self.m_grid_search_result.SetMargins( 0, 0 )
 
         # Columns
-        self.m_grid3.EnableDragColMove( False )
-        self.m_grid3.EnableDragColSize( True )
-        self.m_grid3.SetColLabelAlignment( wx.ALIGN_CENTER, wx.ALIGN_CENTER )
+        self.m_grid_search_result.AutoSizeColumns()
+        self.m_grid_search_result.EnableDragColMove( False )
+        self.m_grid_search_result.EnableDragColSize( False )
+        self.m_grid_search_result.SetColLabelSize( wx.grid.GRID_AUTOSIZE )
+        self.m_grid_search_result.SetColLabelAlignment( wx.ALIGN_CENTER, wx.ALIGN_CENTER )
 
         # Rows
-        self.m_grid3.AutoSizeRows()
-        self.m_grid3.EnableDragRowSize( True )
-        self.m_grid3.SetRowLabelAlignment( wx.ALIGN_CENTER, wx.ALIGN_CENTER )
+        self.m_grid_search_result.AutoSizeRows()
+        self.m_grid_search_result.EnableDragRowSize( False )
+        self.m_grid_search_result.SetRowLabelSize( wx.grid.GRID_AUTOSIZE )
+        self.m_grid_search_result.SetRowLabelAlignment( wx.ALIGN_CENTER, wx.ALIGN_CENTER )
 
         # Label Appearance
 
         # Cell Defaults
-        self.m_grid3.SetDefaultCellAlignment( wx.ALIGN_LEFT, wx.ALIGN_TOP )
-        self.m_grid3.SetMinSize( wx.Size( 450,75 ) )
+        self.m_grid_search_result.SetDefaultCellAlignment( wx.ALIGN_LEFT, wx.ALIGN_TOP )
+        self.m_grid_search_result.SetMinSize( wx.Size( -1,75 ) )
+        self.m_grid_search_result.SetMaxSize( wx.Size( -1,150 ) )
 
-        bSizer1.Add( self.m_grid3, 0, wx.ALL, 5 )
+        bSizer_search_results.Add( self.m_grid_search_result, 0, wx.ALIGN_CENTER_HORIZONTAL|wx.ALL, 5 )
+
+
+        bSizer17.Add( bSizer_search_results, 1, wx.EXPAND, 5 )
+
+
+        self.m_panel_search.SetSizer( bSizer17 )
+        self.m_panel_search.Layout()
+        bSizer17.Fit( self.m_panel_search )
+        bSizer13.Add( self.m_panel_search, 1, wx.EXPAND|wx.LEFT|wx.RIGHT|wx.TOP, 5 )
+
+        self.m_panel_filter = wx.Panel( self, wx.ID_ANY, wx.DefaultPosition, wx.DefaultSize, wx.TAB_TRAVERSAL )
+        bSizer19 = wx.BoxSizer( wx.VERTICAL )
+
+        sbSizer2 = wx.StaticBoxSizer( wx.StaticBox( self.m_panel_filter, wx.ID_ANY, wx.EmptyString ), wx.HORIZONTAL )
+
+        bSizer11 = wx.BoxSizer( wx.VERTICAL )
+
+        self.m_staticText_nutrient_filter = wx.StaticText( sbSizer2.GetStaticBox(), wx.ID_ANY, _(u"Nutrient"), wx.DefaultPosition, wx.DefaultSize, 0 )
+        self.m_staticText_nutrient_filter.Wrap( -1 )
+
+        bSizer11.Add( self.m_staticText_nutrient_filter, 0, wx.ALIGN_CENTER_HORIZONTAL|wx.ALIGN_CENTER_VERTICAL|wx.ALL, 5 )
+
+        m_choice_nutrient_filterChoices = []
+        self.m_choice_nutrient_filter = wx.Choice( sbSizer2.GetStaticBox(), wx.ID_ANY, wx.DefaultPosition, wx.DefaultSize, m_choice_nutrient_filterChoices, 0 )
+        self.m_choice_nutrient_filter.SetSelection( 0 )
+        bSizer11.Add( self.m_choice_nutrient_filter, 0, wx.ALIGN_CENTER_HORIZONTAL|wx.ALIGN_CENTER_VERTICAL|wx.ALL, 5 )
+
+
+        sbSizer2.Add( bSizer11, 1, wx.ALL|wx.EXPAND, 5 )
+
+        bSizer6 = wx.BoxSizer( wx.VERTICAL )
+
+        bSizer7 = wx.BoxSizer( wx.HORIZONTAL )
+
+        self.m_checkBox_level_filter = wx.CheckBox( sbSizer2.GetStaticBox(), wx.ID_ANY, _(u"Level Filter"), wx.DefaultPosition, wx.DefaultSize, 0 )
+        bSizer7.Add( self.m_checkBox_level_filter, 0, wx.ALIGN_CENTER_VERTICAL|wx.ALL, 5 )
+
+        m_choice_filter_levelChoices = [ _(u"Low"), _(u"Mid"), _(u"High") ]
+        self.m_choice_filter_level = wx.Choice( sbSizer2.GetStaticBox(), wx.ID_ANY, wx.DefaultPosition, wx.DefaultSize, m_choice_filter_levelChoices, 0 )
+        self.m_choice_filter_level.SetSelection( 0 )
+        bSizer7.Add( self.m_choice_filter_level, 0, wx.ALIGN_CENTER_VERTICAL|wx.ALL, 5 )
+
+
+        bSizer6.Add( bSizer7, 1, wx.EXPAND, 5 )
+
+        bSizer8 = wx.BoxSizer( wx.HORIZONTAL )
+
+        self.m_checkBox_range_filter = wx.CheckBox( sbSizer2.GetStaticBox(), wx.ID_ANY, _(u"Range Filter"), wx.DefaultPosition, wx.DefaultSize, 0 )
+        bSizer8.Add( self.m_checkBox_range_filter, 0, wx.ALIGN_CENTER_VERTICAL|wx.ALL, 5 )
+
+        bSizer9 = wx.BoxSizer( wx.VERTICAL )
+
+        self.m_staticText_range_min = wx.StaticText( sbSizer2.GetStaticBox(), wx.ID_ANY, _(u"Min"), wx.DefaultPosition, wx.DefaultSize, 0 )
+        self.m_staticText_range_min.Wrap( -1 )
+
+        bSizer9.Add( self.m_staticText_range_min, 0, wx.ALIGN_CENTER_HORIZONTAL|wx.ALL, 5 )
+
+        self.m_spinCtrlDouble_range_min = wx.SpinCtrlDouble( sbSizer2.GetStaticBox(), wx.ID_ANY, wx.EmptyString, wx.DefaultPosition, wx.DefaultSize, wx.SP_ARROW_KEYS, 0, 100, 0, 1 )
+        self.m_spinCtrlDouble_range_min.SetDigits( 0 )
+        bSizer9.Add( self.m_spinCtrlDouble_range_min, 0, wx.ALL, 5 )
+
+
+        bSizer8.Add( bSizer9, 1, wx.EXPAND, 5 )
+
+        bSizer10 = wx.BoxSizer( wx.VERTICAL )
+
+        self.m_staticText_range_max = wx.StaticText( sbSizer2.GetStaticBox(), wx.ID_ANY, _(u"Max"), wx.DefaultPosition, wx.DefaultSize, 0 )
+        self.m_staticText_range_max.Wrap( -1 )
+
+        bSizer10.Add( self.m_staticText_range_max, 0, wx.ALIGN_CENTER_HORIZONTAL|wx.ALL, 5 )
+
+        self.m_spinCtrlDouble_range_max = wx.SpinCtrlDouble( sbSizer2.GetStaticBox(), wx.ID_ANY, wx.EmptyString, wx.DefaultPosition, wx.DefaultSize, wx.SP_ARROW_KEYS, 0, 100, 0, 1 )
+        self.m_spinCtrlDouble_range_max.SetDigits( 0 )
+        bSizer10.Add( self.m_spinCtrlDouble_range_max, 0, wx.ALL, 5 )
+
+
+        bSizer8.Add( bSizer10, 1, wx.EXPAND, 5 )
+
+
+        bSizer6.Add( bSizer8, 1, wx.EXPAND, 5 )
+
+
+        sbSizer2.Add( bSizer6, 1, wx.EXPAND, 5 )
+
+        self.m_button_enable_searching = wx.Button( sbSizer2.GetStaticBox(), wx.ID_ANY, _(u"Enable Searching"), wx.DefaultPosition, wx.DefaultSize, 0 )
+        sbSizer2.Add( self.m_button_enable_searching, 0, wx.ALIGN_CENTER_VERTICAL|wx.ALL, 5 )
+
+
+        bSizer19.Add( sbSizer2, 1, 0, 5 )
+
+        bSizer15 = wx.BoxSizer( wx.VERTICAL )
+
+        self.m_staticText_filter_result = wx.StaticText( self.m_panel_filter, wx.ID_ANY, _(u"Nutrients"), wx.DefaultPosition, wx.DefaultSize, 0 )
+        self.m_staticText_filter_result.Wrap( -1 )
+
+        bSizer15.Add( self.m_staticText_filter_result, 0, wx.ALIGN_CENTER_HORIZONTAL|wx.ALL, 5 )
+
+        self.m_grid_filter_result = wx.grid.Grid( self.m_panel_filter, wx.ID_ANY, wx.DefaultPosition, wx.DefaultSize, 0 )
+
+        # Grid
+        self.m_grid_filter_result.CreateGrid( 1, 5 )
+        self.m_grid_filter_result.EnableEditing( False )
+        self.m_grid_filter_result.EnableGridLines( True )
+        self.m_grid_filter_result.EnableDragGridSize( False )
+        self.m_grid_filter_result.SetMargins( 0, 0 )
+
+        # Columns
+        self.m_grid_filter_result.AutoSizeColumns()
+        self.m_grid_filter_result.EnableDragColMove( False )
+        self.m_grid_filter_result.EnableDragColSize( False )
+        self.m_grid_filter_result.SetColLabelSize( wx.grid.GRID_AUTOSIZE )
+        self.m_grid_filter_result.SetColLabelAlignment( wx.ALIGN_CENTER, wx.ALIGN_CENTER )
+
+        # Rows
+        self.m_grid_filter_result.AutoSizeRows()
+        self.m_grid_filter_result.EnableDragRowSize( False )
+        self.m_grid_filter_result.SetRowLabelSize( wx.grid.GRID_AUTOSIZE )
+        self.m_grid_filter_result.SetRowLabelAlignment( wx.ALIGN_CENTER, wx.ALIGN_CENTER )
+
+        # Label Appearance
+
+        # Cell Defaults
+        self.m_grid_filter_result.SetDefaultCellAlignment( wx.ALIGN_LEFT, wx.ALIGN_TOP )
+        self.m_grid_filter_result.SetMinSize( wx.Size( -1,75 ) )
+        self.m_grid_filter_result.SetMaxSize( wx.Size( -1,150 ) )
+
+        bSizer15.Add( self.m_grid_filter_result, 0, wx.ALIGN_CENTER_HORIZONTAL|wx.ALL, 5 )
+
+
+        bSizer19.Add( bSizer15, 1, wx.EXPAND, 5 )
+
+
+        self.m_panel_filter.SetSizer( bSizer19 )
+        self.m_panel_filter.Layout()
+        bSizer19.Fit( self.m_panel_filter )
+        bSizer13.Add( self.m_panel_filter, 1, wx.EXPAND |wx.ALL, 5 )
+
+
+        fgSizer3.Add( bSizer13, 1, wx.EXPAND, 5 )
+
+
+        bSizer1.Add( fgSizer3, 1, 0, 5 )
 
 
         self.SetSizer( bSizer1 )
